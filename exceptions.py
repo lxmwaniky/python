f6d1f14a3a4624547dfa1a3227d3dd79ae98f800
Alex Mwaniki <lekko254@gmail.com>
2023-07-20 11:06:11 +0300

    get_int function

--- exceptions.py
+++ exceptions.py
@@ -1,8 +1,15 @@
-while True:
-    try:
-        x = int(input("Enter a value for X: "))
-    except ValueError:
-        print("x should be an integer")
-    else:
-        break
-print(f"x is {x}")
+def main():
+     x = get_int()
+     print(f"x is {x}")
+
+def get_int():
+    while True:
+            try:
+                x = int(input("Enter a value for X: "))
+            except ValueError:
+                print("x should be an integer")
+            else:
+                break
+    return x
+
+main()
