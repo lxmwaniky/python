7f2ff00ae28cfe1bb16bcaf67b68d83a854d40e3
Alex Mwaniki <lekko254@gmail.com>
2023-07-20 08:45:43 +0300

    introduced a While loop to continue prompting the user to enter a valid value(integer)

--- exceptions.py
+++ exceptions.py
@@ -1,5 +1,8 @@
-try:
-    x = int(input("Enter a value for X: "))
-    print(f"x is {x}")
-except ValueError:
-    print("x should be an integer")
+while True:
+    try:
+        x = int(input("Enter a value for X: "))
+    except ValueError:
+        print("x should be an integer")
+    else:
+        break
+print(f"x is {x}")
