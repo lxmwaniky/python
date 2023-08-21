d5283cda8cd20f4260f23e10b7cc563818abed94
Alex Mwaniki <lekko254@gmail.com>
2023-08-22 01:18:02 +0300

    Button

---
+++ tkinter/button.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+count = 0
+def click():
+    global count
+    count+=1
+    print(count)
+
+window = Tk()
+
+button = Button(window, text="Click me", command=click, font=("Comic Sans",30))
+button.pack()
+
+window.mainloop()
